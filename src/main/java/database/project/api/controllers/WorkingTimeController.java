01cd4ba6b98b6fe41bcfd4036becc5340bc404cb
kadriacibadem <kadriacibadem@hotmail.com>
2021-12-19 17:59:30 +0300

    fixed workingtime delete operation

--- src/main/java/database/project/api/controllers/WorkingTimeController.java
+++ src/main/java/database/project/api/controllers/WorkingTimeController.java
@@ -1,7 +1,9 @@
 package database.project.api.controllers;
 
+import database.project.business.abstracts.DiseaseService;
 import database.project.business.abstracts.WorkingTimeService;
 import database.project.core.utilites.results.Result;
+import database.project.entites.concretes.Disease;
 import database.project.entites.concretes.WorkingTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,24 +12,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/workingtime")
+@RequestMapping("/api/working")
 public class WorkingTimeController {
 
     @Autowired
-    public WorkingTimeController(WorkingTimeService workingTimeService){
+    public WorkingTimeController(WorkingTimeService workingTimeService) {
         this.workingTimeService = workingTimeService;
     }
 
     private WorkingTimeService workingTimeService;
 
-    @PostMapping(name = "/add")
+
+    @PostMapping("/add")
     public Result add(@RequestBody WorkingTime workingTime){
         return this.workingTimeService.add(workingTime);
     }
 
-   /* @PostMapping(name = "/delete")
+    @PostMapping("/delete")
     public Result delete(@RequestBody WorkingTime workingTime){
         return this.workingTimeService.delete(workingTime);
-    } */
+    }
 
 }
